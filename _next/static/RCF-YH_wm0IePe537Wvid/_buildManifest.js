61fcedb027319459b2b399bc548d6d479d01cb57
mikhail russo <mikhail.russo2015@gmail.com>
2026-03-12 12:09:33 -0400

    Updates

---
+++ _next/static/RCF-YH_wm0IePe537Wvid/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/insanity-media//_next/:path+",
+        "destination": "/insanity-media/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
